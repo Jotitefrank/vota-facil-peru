a797963cee6cba8e63ac9e69f0ec12c3fef5420f
John <100967480+Jotitefrank@users.noreply.github.com>
2025-11-14 19:24:54 -0500

    first commit

---
+++ src/main/java/com/devinnovator/vota_facil_peru/VotaFacilPeruApplication.java
@@ -0,0 +1,13 @@
+package com.devinnovator.vota_facil_peru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotaFacilPeruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotaFacilPeruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devinnovator/vota_facil_peru/VotaFacilPeruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinnovator.vota_facil_peru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotaFacilPeruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
